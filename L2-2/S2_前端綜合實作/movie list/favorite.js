941fd90e5e49927eb3393b97546f64cbf65a2ddc
elliot <elliotcs30@gmail.com>
2022-06-19 17:01:38 +0800

    feat: remove item from facorite list

--- L2-2/S2_前端綜合實作/movie list/favorite.js
+++ L2-2/S2_前端綜合實作/movie list/favorite.js
@@ -23,7 +23,7 @@ function renderMovieList(data) {
             </div>
             <div class="card-footer">
               <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-model" data-bs-id="${item.id}">More</button>
-              <button class="btn btn-info btn-add-favorite" data-bs-id="${item.id}">+</button>
+              <button class="btn btn-danger btn-remove-favorite" data-bs-id="${item.id}">X</button>
             </div>
           </div>
         </div>
@@ -50,13 +50,23 @@ function showMovieModal(id) {
     })
 }
 
+function removeFromFavorite(id) {
+  if (!movies) return
+  
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+  
+  if (!movies === -1) return
 
+  movies.splice(movieIndex, 1)
+  localStorage.setItem('favoriteMovies', JSON.stringify(movies))
+  renderMovieList(movies)
+}
 
 dataPanel.addEventListener('click', function onPanelClicked(event) {
   if (event.target.matches('.btn-show-movie')) {
     showMovieModal(Number(event.target.dataset.bsId))
-  } else if (event.target.matches('.btn-add-favorite')) {
-    addToFavorite(Number(event.target.dataset.bsId))
+  } else if (event.target.matches('.btn-remove-favorite')) {
+    removeFromFavorite(Number(event.target.dataset.bsId))
   }
 })
 
